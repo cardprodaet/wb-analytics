a3cdd6f43ad10c50c1e40adc4fb4aaa65739c8cc
Владислав Гвоздев <vladislavgvozdev@MacBook-Air-Vladislav.local>
2026-05-15 13:18:34 +0300

    Increase ADV_SLEEP to 90s to avoid sustained 429 rate limiting

--- main2.py
+++ main2.py
@@ -27,7 +27,7 @@ ADV_BASE       = 'https://advert-api.wildberries.ru'
 ANALYTICS_BASE = 'https://seller-analytics-api.wildberries.ru'
 
 CAMP_CHUNK  = 50
-ADV_SLEEP   = 65
+ADV_SLEEP   = 90
 PAGE_SLEEP  = 20
 WRITE_BATCH = 500
 
